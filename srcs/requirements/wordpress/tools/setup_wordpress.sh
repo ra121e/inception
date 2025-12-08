d1f88e2dcf6b3a28be62daf298cccecbca1ec071
Atsushi Honda <athonda@c2r5s15.42singapore.sg>
2025-12-08 22:20:43 +0800

    fix: 8443 setup shell script nu I have to delete previous wp-config.php

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -3,6 +3,9 @@ set -e
 
 WP_PATH="/var/www/html/wordpress"
 
+# .env から来る DOMAIN_NAME。なければ localhost にする
+DOMAIN_NAME=${DOMAIN_NAME:-localhost}
+
 # DB 接続情報（.env と secrets から）
 DB_HOST=${DB_HOST:-mariadb}
 DB_PORT=${DB_PORT:-3306}
@@ -65,73 +68,3 @@ fi
 
 echo "Starting php-fpm..."
 exec "$@"
-
-
-# #!/bin/bash
-# set -e
-
-# WP_DIR="/var/www/html/wordpress"
-# WP_CONFIG="${WP_DIR}/wp-config.php"
-
-
-# export WP_MEMORY_LIMIT=512M
-# php -d memory_limit=512M /var/www/html/wp-cli.phar core download --allow-root
-# php -d memory_limit=512M /var/www/html/wp-cli.phar config create \
-#     --dbname="$WORDPRESS_DB_NAME" \
-#     --dbuser="$WORDPRESS_DB_USER" \
-#     --dbpass="$WORDPRESS_DB_PASSWORD" \
-#     --dbhost="$WORDPRESS_DB_HOST" \
-#     --allow-root
-# php -d memory_limit=512M /var/www/html/wp-cli.phar core install \
-#     --url=localhost \
-#     --title=inception \
-#     --admin_user=admin \
-#     --admin_password=admin \
-#     --admin_email=admin@admin.com \
-#     --allow-root
-
-# cd /var/www/html
-# curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# chmod +x wp-cli.phar
-# ./wp-cli.phar core download --allow-root
-# ./wp-cli.phar config create --dbname=wordpress --dbuser=wordpress --dbpass=wordpress --dbhost=db --allow-root
-# ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-
-
-
-# # # 初回のみ設定
-# # if [ ! -f "$WP_CONFIG" ]; then
-# #     echo "WordPress 初期設定を開始します..."
-
-# #     # 確実にサンプルファイルが存在するかチェック
-# #     if [ ! -f "$WP_DIR/wp-config-sample.php" ]; then
-# #         echo "Error: wp-config-sample.php が見つかりません: $WP_DIR"
-# #         exit 1
-# #     fi
-
-
-# #     # 必須環境変数チェック
-# #     : "${WORDPRESS_DB_HOST:?WORDPRESS_DB_HOSTが未設定です}"
-# #     : "${WORDPRESS_DB_NAME:?WORDPRESS_DB_NAMEが未設定です}"
-# #     : "${WORDPRESS_DB_USER:?WORDPRESS_DB_USERが未設定です}"
-# #     : "${WORDPRESS_DB_PASSWORD:?WORDPRESS_DB_PASSWORDが未設定です}"
-
-# #     # wp-config.php生成
-# #     cp "${WP_DIR}/wp-config-sample.php" "$WP_CONFIG"
-# #     sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" "$WP_CONFIG"
-# #     sed -i "s/username_here/$WORDPRESS_DB_USER/" "$WP_CONFIG"
-# #     sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" "$WP_CONFIG"
-# #     sed -i "s/localhost/$WORDPRESS_DB_HOST/" "$WP_CONFIG"
-
-# #     # 権限設定
-# #     chown -R nobody:nobody "$WP_DIR"
-
-# #     echo "WordPress の初期設定が完了しました。"
-# # else
-# #     echo "WordPress はすでに設定済みです。"
-# # fi
-
-# # CMD引数（php-fpm）をフォアグラウンドで実行
-# # exec "$@"
-
-# php-fpm8.1 -F
